e5e6dbd1065fc6f95b82f7641c777b91a0e247f7
vanstee <patrick@vanstee.me>
2016-01-14 20:59:57 -0800

    Rework bundle actions to use human args and output

---
+++ lib/cogctl/actions/bundles.ex
@@ -0,0 +1,34 @@
+defmodule Cogctl.Actions.Bundles do
+  use Cogctl.Action, "bundles"
+  alias Cogctl.CogApi
+  alias Cogctl.Table
+
+  def option_spec do
+    []
+  end
+
+  def run(_options, _args, _config, profile) do
+    client = CogApi.new_client(profile)
+    case CogApi.authenticate(client) do
+      {:ok, client} ->
+        do_list(client)
+      {:error, error} ->
+        IO.puts "#{error["error"]}"
+    end
+  end
+
+  def do_list(client) do
+    case CogApi.bundle_index(client) do
+      {:ok, resp} ->
+        bundles = for bundle <- resp["bundles"] do
+          [bundle["name"], bundle["inserted_at"]]
+        end
+
+        IO.puts(Table.format([["NAME", "INSTALLED"]] ++ bundles))
+
+        :ok
+      {:error, resp} ->
+        {:error, resp}
+    end
+  end
+end
