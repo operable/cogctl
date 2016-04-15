f48a8365f53f4a06f2ed74db965e7da0012fd6e8
Matthew Peck <matt@operable.io>
2016-04-15 12:19:33 -0400

    add support for bulk unassigning

--- lib/cogctl/actions/relay_groups/unassign.ex
+++ lib/cogctl/actions/relay_groups/unassign.ex
@@ -1,28 +1,36 @@
 defmodule Cogctl.Actions.RelayGroups.Unassign do
   use Cogctl.Action, "relay-groups unassign"
 
+  @moduledoc """
+  Unassigns bundles to relay groups. Requires the group and at least one bundle to
+  work properly. Iterates over the list of bundles and removes the assignment from
+  the relay-group. If an error occurs, the operation is aborted and an error message
+  returned to the user.
+  """
+
   def option_spec() do
-    [{:name, :undefined, :undefined, {:string, :undefined}, 'Relay Group name (required)'},
-     {:bundle, :undefined, 'bundle', {:string, :undefined}, 'Bundle name (required)'}]
+    [{:relay_group, :undefined, :undefined, {:string, :undefined}, 'Relay Group name (required)'},
+     # Technically this will just be the first bundle
+     {:bundles, :undefined, :undefined, {:string, :undefined}, 'Bundle names (required)'}]
   end
 
-  def run(options, _args, _config, %{token: nil}=endpoint) do
-    with_authentication(endpoint, &run(options, nil, nil, &1))
-  end
-  def run(options, _args, _config, endpoint) do
-    case convert_to_params(options, [bundle: :required,
-                                     name: :required]) do
+  def run(options, args, _config, endpoint) do
+    # At least one bundle is required, so we specify that here
+    case convert_to_params(options, [relay_group: :required,
+                                     bundles: :required]) do
       {:ok, params} ->
-        do_unassign(endpoint, params)
-      _ ->
+        # The rest of the bundles, if there are any, get appended here.
+        params = %{params | bundles: [params.bundles | args]}
+        with_authentication(endpoint, &do_unassign(&1, params))
+      :error ->
         display_arguments_error
     end
   end
 
   defp do_unassign(endpoint, params) do
-    case CogApi.HTTP.Client.relay_group_remove_bundle(%{name: params.name}, %{bundle: params.bundle}, endpoint) do
+    case CogApi.HTTP.Client.relay_group_remove_bundles(%{name: params.relay_group}, %{bundles: params.bundles}, endpoint) do
       {:ok, _} ->
-        display_output("Bundle `#{params.bundle}` unassigned from relay group `#{params.name}`")
+        display_output("Unassigned '#{Enum.join(params.bundles, ", ")}' from relay group `#{params.relay_group}`")
       {:error, error} ->
         display_error(error)
     end
