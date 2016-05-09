1a75fe4d7e8f990d9264e3182bd3dae9cec05c56
Matthew Peck <matt@operable.io>
2016-05-09 10:18:37 -0400

    fix test

--- test/cogctl_test.exs
+++ test/cogctl_test.exs
@@ -465,7 +465,7 @@ defmodule CogctlTest do
 
     assert run("cogctl relays delete test-relay mimimi") =~ ~r"""
     Deleted 'test-relay'
-    cogctl: ERROR: The relay `mimimi` could not be deleted: Resource not found for: 'relays'
+    cogctl: ERROR: "The relay `mimimi` could not be deleted: Resource not found for: 'relays'"
     """
 
     run("cogctl relay-groups create mygroup")
