a01eb8d1e91231c1dcf70b84b216904706ab1ba4
Kevin Smith <kevin@operable.io>
2016-08-22 09:35:52 -0400

    Revert "Bump version for next release"
    This reverts commit 749589bad6f7bfed33f43c4f8fb0c30c2be8f037.

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Cogctl.Mixfile do
 
   def project do
     [app: :cogctl,
-     version: "0.14.0",
+     version: "0.13.0",
      elixir: "~> 1.3.1",
      elixirc_options: [warnings_as_errors: System.get_env("ALLOW_WARNINGS") == nil],
      build_embedded: Mix.env == :prod,
