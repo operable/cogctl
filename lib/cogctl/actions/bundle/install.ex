57203445c35375da6cee1f812fc5a7be62c7e7f9
Matthew Peck <matt@operable.io>
2016-10-03 11:32:07 -0400

    Support the new style templates with the --templates flag
    Note that this removes support for the old adapter specific means of
    specifying templates.

--- lib/cogctl/actions/bundle/install.ex
+++ lib/cogctl/actions/bundle/install.ex
@@ -5,26 +5,19 @@ defmodule Cogctl.Actions.Bundle.Install do
   alias Cogctl.Table
   import Cogctl.Actions.Bundle.Helpers, only: [field: 2]
 
-  @template_extension ".mustache"
+  @template_extensions [".md", ".greenbar"]
 
   @moduledoc """
   Creates bundles. The bundles create action creates bundles by taking a bundle
   config file, running some validations on it and POSTing the resulting json.
   Special considerations are made for templates. Templates can be inlined in the
-  bundle config or split up into separate files following a specific file hierachy.
-  In the later case this action will read each template and append the contents to
-  the config file.
+  bundle config or split up into separate files. By default cogctl will look for
+  a 'templates' directory in the current working directory. Optionally a user can
+  specify a path for templates using the --templates switch.
 
-  The file hierachy looks as follows:
-    templates
-    └── <adapter>
-        └── <template_name>.mustache
-
-  Your template name should correspond to the command that it belongs to. For example,
-  a command called 'date' would have a template called 'date.mustache'
-
-  By default cogctl will look for a directory called 'templates' in the working directory.
-  Optionally you can pass a directory via the '--templates' option.
+  Template names should correspond to the command that they belong to. For example,
+  a command called 'date' would have a template called 'date.greenbar'. Template
+  files may use either the '.greenbar' or the '.md' extension.
   """
 
   def option_spec do
@@ -108,11 +101,9 @@ defmodule Cogctl.Actions.Bundle.Install do
   defp install_bundle(endpoint, params) do
     case parse_bundle_or_path(params) do
       {:config, config} ->
-        with {:ok, templates}      <- build_template_map(params.templates),
-             {:ok, amended_config} <- maybe_add_templates(templates, config),
-             {:ok, fixed_config}   <- validate_config(amended_config) do
-          params = Map.put(params, "config", fixed_config)
-          Client.bundle_install(endpoint, params)
+        with {:ok, amended_config}  <- add_templates_from_dir(config, params.templates),
+             {:ok, valid_config}    <- validate_config(amended_config) do
+          Client.bundle_install(endpoint, %{"config" => valid_config})
         end
       {:registry, {bundle, version}} ->
         Client.bundle_install_from_registry(endpoint, bundle, version)
@@ -152,57 +143,32 @@ defmodule Cogctl.Actions.Bundle.Install do
     end
   end
 
-  defp build_template_map(template_dir) do
+  defp add_templates_from_dir(config, template_dir) do
     path = Path.relative_to_cwd(template_dir)
-    with {:ok, adapters} <- adapters(path) do
-      templates(path, adapters)
-    end
-  end
-
-  defp adapters(path) do
     if File.dir?(path) do
-      adapters = File.ls!(path)
-      |> Enum.filter(&File.dir?(Path.join(path, &1)))
-      {:ok, adapters}
+      templates = File.ls!(path)
+      |> Enum.filter(&String.ends_with?(&1, @template_extensions))
+      |> Map.new(&({template_name(&1), template_body(path, &1)}))
+      |> Map.merge(Map.get(config, "templates", %{}))
+
+      {:ok, put_in(config, ["templates"], templates)}
     else
-      {:ok, []}
+      {:ok, config}
     end
   end
 
-  defp templates(path, adapters) do
-    # Produces a map like this:
-    # %{<template_name> => %{
-    #     <adapter> => <template_content>,
-    #     <adapter> => <template_content>},
-    #   <template_name> => %{
-    #     ...}
-    #  }
-    templates = Enum.reduce(adapters, %{}, fn(adapter, acc) ->
-      Path.join(path, adapter)
-      |> File.ls!
-      |> Enum.filter(&template?/1)
-      |> Enum.reduce(acc, &insert_template(&2, Path.join([path, adapter, &1]), adapter))
-    end)
-    {:ok, templates}
+  defp template_name(template) do
+    ext = Path.extname(template)
+    Path.basename(template, ext)
   end
 
-  defp template?(file_name),
-    do: String.ends_with?(file_name, @template_extension)
-
-  defp insert_template(map, path, adapter) do
-    template_name = Path.basename(path, @template_extension)
-    template = File.read!(path)
-
-    if Map.has_key?(map, template_name) do
-      put_in(map, [template_name, adapter], template)
-    else
-      Map.put(map, template_name, %{adapter => template})
-    end
+  defp template_body(path, filename) do
+    %{"body" => read_template!(path, filename)}
   end
 
-  defp maybe_add_templates(external_templates, config) do
-    templates = Map.merge(external_templates, Map.get(config, "templates", %{}))
-    {:ok, Map.put(config, "templates", templates)}
+  defp read_template!(path, filename) do
+    Path.join([path, filename])
+    |> File.read!
   end
 
   defp format_validation_reason(reason, field_path) do
