a7906f5c4120ae55e795e6402e4b41976e455452
Kevin Smith <kevin@operable.io>
2017-02-16 11:05:45 -0500

    Added git tag info to cogctl version

--- cogctl/cli/version.py
+++ cogctl/cli/version.py
@@ -9,7 +9,7 @@ def version():
     """
     Display version and build information.
     """
-    version_string = "cogctl %s (build: %s)" % (cogctl.__version__,
+    version_string = "cogctl %s (build: %s)" % (get_build_tag(),
                                                 get_build_version())
     click.echo(version_string)
 
@@ -19,22 +19,39 @@ def get_build_version():
     Return the contents of the file `cogctl/GITSHA`. This is generated
     at build-time and contains the truncated Git SHA of the code being
     built.
+    """
+
+    return _load_bundled_file('GITSHA')
+
+
+def get_build_tag():
+    """
+    Return the contents of the file `cogctl/GITTAG`. This is generated
+    at build-time and contains the Git tag or branch name of the code
+    being built. If no file is found then the string `'dev'` is used.
+    """
+
+    return _load_bundled_file('GITTAG', default='dev')
+
+
+def _load_bundled_file(name, default='unknown'):
+    """
+    Return the contents of a bundled file.
 
     If running in the context of a PyInstaller-built binary, there
     will be a `frozen` attribute on the `sys` module. When running
-    inside such a binary, we'll need to resolve the location of the
-    GITSHA file relative to that bundle.
+    inside such a binary, we'll need to resolve the file's location
+    relative to that bundle.
     """
-
     if getattr(sys, 'frozen', False):
         bundle_dir = sys._MEIPASS
     else:
         bundle_dir = os.path.dirname(cogctl.__file__)
 
-    filename = os.path.join(bundle_dir, 'GITSHA')
+    filename = os.path.join(bundle_dir, name)
 
     if not os.path.exists(filename):
-        return 'unknown'
+        return default
 
     with open(filename) as fh:
         return fh.read().strip()
